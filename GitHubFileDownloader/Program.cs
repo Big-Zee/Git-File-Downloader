80035c132792b987ef156cf1828d006ce8ab09dc
Zbig <Zbig@zbig>
2019-02-06 22:14:08 +0100

    Change solution to .Net 4.6, added references to library, added small snippet into Program.cs

--- GitHubFileDownloader/Program.cs
+++ GitHubFileDownloader/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GitHubFileDownloader
 {
@@ -6,7 +12,28 @@ namespace GitHubFileDownloader
     {
         static void Main(string[] args)
         {
-            
+            var repo = new Repository("https://github.com/zamipl/Git-File-Downloader/blob/master/README.md");
+            foreach (var item in repo.RetrieveStatus())
+            {
+                if (item.State == FileStatus.ModifiedInIndex)
+                {
+                    var blob = repo.Head.Tip[item.FilePath].Target as Blob;
+                    string commitContent;
+                    using (var content = new StreamReader(blob.GetContentStream(), Encoding.UTF8))
+                    {
+                        commitContent = content.ReadToEnd();
+                    }
+                    string workingContent;
+                    using (var content = new StreamReader(repo.Info.WorkingDirectory + Path.DirectorySeparatorChar + item.FilePath, Encoding.UTF8))
+                    {
+                        workingContent = content.ReadToEnd();
+                    }
+                    Console.WriteLine("\n\n~~~~ Original file ~~~~");
+                    Console.WriteLine(commitContent);
+                    Console.WriteLine("\n\n~~~~ Current file ~~~~");
+                    Console.WriteLine(workingContent);
+                }
+            }
         }
     }
 }
